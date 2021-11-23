cfc02320deb454608881ec7e212519543dbb1f45
Goliash <Goliash@KONSTRUKTOR4>
2021-11-23 08:13:53 +0500

    Добавьте файлы проекта.

---
+++ Task_5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7; //число элементов массива
+            int[] array = new int[n]; //инициализация одномерного массива c n элементами
+            int i = 0;
+            int sum = 0;
+             while (i < n)
+            {
+                Console.WriteLine("Введите элемент массива:");
+                //Ввод элементов массива с клавиатуры
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                i++;
+            }
+            
+            for (i = 0; i < n; i++) //вывод массива
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            
+            for (i = 0; i < n; i++) //суммирование элементов массива
+            {
+               sum += array[i];
+            }
+            float srA = (float)sum/n; //подсчет среднего арифметического элементов массива
+            Console.WriteLine("Среднеарифметическое элементов массива равно {0:0.00}", srA); // вывод результата
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_5.1_v2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._1_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7]; 
+            Console.WriteLine("Введите первое число:");
+            array[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            array[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число:");
+            array[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите четвертое число:");
+            array[3] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите пятое число:");
+            array[4] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите шестое число:");
+            array[5] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите седьмое число:");
+            array[6] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
